7a08090478ad7743373cbcac0a72cf1f32c0eb99
Ivan <redial@abv.bg>
2021-12-19 22:47:46 +0200

    part two

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 def main():
+    pass
+
+
+def read_input():
     with open('input.txt') as file:
         lines = file.readlines()
         lines = [int(line.rstrip()) for line in lines]
-    result = [i for i in range(1, len(lines)) if lines[i] > lines[i-1]] 
+        return lines
 
+
+def part_one(input):
+    result = [i for i in range(1, len(input)) if input[i] > input[i-1]] 
     return len(result)
 
 
+def part_two(input):
+    total = []
+
+    for i in range(len(input)-2):
+        total.append(sum(input[i:i+3]))
+
+    return part_one(total)
+
+
 if __name__ == '__main__':
-    print(main())
+    print(part_two(read_input()))
